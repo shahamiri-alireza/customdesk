0738b5ec139eabb8f54bb9d62d59b59cac5b09bc
Alireza Shahamiri <shahamiri.alireza81@gmail.com>
2024-12-31 14:36:00 +0330

    fix

--- flutter/lib/models/platform_model.dart
+++ flutter/lib/models/platform_model.dart
@@ -5,4 +5,4 @@ import 'package:flutter_hbb/generated_bridge.dart'
 final platformFFI = PlatformFFI.instance;
 final localeName = PlatformFFI.localeName;
 
-NikanDeskImpl get bind => platformFFI.ffiBind;
+NikandeskImpl get bind => platformFFI.ffiBind;
